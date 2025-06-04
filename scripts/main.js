bb146938f41282b423aec2547e36074d79d38732
FabianMarsh <fabian.marsh00@gmail.com>
2025-06-04 17:18:00 +0000

    refined styles

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,11 @@ const swiperIcons = new Swiper('.swiper__icons', {
     centeredSlides: true,
     slideToClickedSlide: true,
     spaceBetween: 50,
+    initialSlide: 2,
     breakpoints: {
         480: { slidesPerView: 2, spaceBetween: 15 },
         768: { slidesPerView: 3, spaceBetween: 20 },
-        1024: { slidesPerView: 5, spaceBetween: 30 },
+        1250: { slidesPerView: 5, spaceBetween: 30 },
     },
     navigation: {
         nextEl: '.swiper-button-next',
@@ -18,6 +19,7 @@ const swiperIcons = new Swiper('.swiper__icons', {
 const swiperCards = new Swiper('.swiper__cards', {
     slidesPerView: 1,
     spaceBetween: 100,
+    initialSlide: 2,
 });
 
 // Sync both Swipers
